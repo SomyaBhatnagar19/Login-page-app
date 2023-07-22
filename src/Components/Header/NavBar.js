4e11d891e854ce2b9599aef3f3267471c6d99456
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-07-22 10:58:48 +0530

    Corrections in Login page component.

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import { Navbar, Container, Nav, Button } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// // import './NavBar.css';
-
-// const NavBar = () => {
-//   return (
-//     <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand as={Link} to="/home">
-//           RactAuth
-//         </Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="ms-auto">
-//             <Nav.Link as={Link} to="/login">
-//               Login
-//             </Nav.Link>
-//             <Nav.Link as={Link} to="/profile">
-//               Profile
-//             </Nav.Link>
-//             <Nav.Link>
-//               <Button>Logout</Button>
-//             </Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
-// export default NavBar;
 import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -39,7 +7,7 @@ const NavBar = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand as={Link} to="/HomePage">
-          RactAuth
+          ReactAuth
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- src/Components/Pages/Login/LoginPage.js
+++ src/Components/Pages/Login/LoginPage.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Card, Form, Button } from "react-bootstrap";
+
 const LoginPage = () => {
-    return(
-        <h1>Im Login Page</h1>
-    )
+  const [isLogin, setIsLogin] = useState(false);
+  const [isExistingUser, setIsExistingUser] = useState(false);
+
+  const existingAccHandler = () => {
+    setIsExistingUser((prevState)=>!prevState);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setIsLogin(true);
+  }
+
+  return(
+    <Container>
+      <Card>
+        <Card.Header>
+          {isLogin || isExistingUser ? <h4>Sign In</h4>: <h4>Sign Up</h4>}
+        </Card.Header>
+        <Form onSubmit={submitHandler}>
+        <Card.Body>
+          
+          <Form.Group>
+            <Form.Label>
+              <h6>Your Email: </h6>
+            </Form.Label>
+            <Form.Control
+              type="email"/>
+          </Form.Group>
+          <Form.Label>
+              <h6>Your Password: </h6>
+            </Form.Label>
+            <Form.Control
+              type="password"/>
+              {isLogin || isExistingUser ? (
+                <Button>
+                  Log In
+                </Button>
+              ):(
+                <Button>
+                  Create New Account
+                </Button>
+              )}
+              </Card.Body>
+              <Card.Footer>
+              {!isExistingUser ? (<p onClick={existingAccHandler}>Log in with existing account</p>):(<p onClick={existingAccHandler}>New User?</p>)}
+              </Card.Footer>
+              
+          </Form>
+        
+      </Card>
+    </Container>
+  )
 }
 export default LoginPage;
