4f2d6883874cf926e8e7ac72055f6494c103ae78
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-07-24 18:42:34 +0530

    Adding the functionality to logout button and on pressing it replacing the token with empty string and also redirecting user to login page.

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -3,13 +3,19 @@ import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../Store/AuthContext";
-
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
   const AuthCtx = useContext(AuthContext);
   
   //!! -> used to make the string changed to boolean
   const isLoggedIn = !!AuthCtx.token;
-  
+  const navigate = useNavigate(); //using it to set the loginpage when the logout button is clicked
+
+  const logoutHandler = () => {
+    AuthCtx.logout();
+    alert('User Successfully logged out.');
+    navigate('/LoginPage');
+  }
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -26,7 +32,7 @@ const NavBar = () => {
               Profile
             </Nav.Link>}
             {isLoggedIn && <Nav.Link>
-              <Button>Logout</Button>
+              <Button onClick={logoutHandler}>Logout</Button>
             </Nav.Link>}
           </Nav>
         </Navbar.Collapse>
